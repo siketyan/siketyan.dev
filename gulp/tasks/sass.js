a4ff56687db6d466e787d9642c7c5f8c5f28f42c
Naoki Ikeguchi <root@siketyan.dev>
2020-11-20 00:38:01 +0900

    🔧 Fix importing files in a package

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -6,6 +6,11 @@ sass.compiler = require('sass');
 
 gulp.task("sass", function () {
     return gulp.src(config.sass.src)
-        .pipe(sass()).on("error", sass.logError)
+        .pipe(sass({
+            includePaths: [
+              'node_modules',
+            ],
+        }))
+        .on("error", sass.logError)
         .pipe(gulp.dest(config.sass.dest));
 });
